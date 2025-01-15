dda8e5b58555bd7a4c7c422bd2a039213023b027
Iivari Anttila <iivari03@gmail.com>
2025-01-15 22:05:05 +0200

    Tasks 4 and 5

---
+++ Assignment 1/Task4_Task5.py
@@ -0,0 +1,53 @@
+# Task 4
+def check_input(target:str, target_type:type):
+    # Check if input matches the target type
+    try:
+        target_type(target)
+        return True
+    except ValueError:
+        return False
+
+def get_input():
+    # Get an input and check if it's an integer
+    user_input = input("Enter an integer or input 0 to exit: ")
+    while True:
+        is_valid = user_input == "0" or check_input(user_input, int)
+        if is_valid:
+            return int(user_input)
+
+        user_input = input("Input not valid (integer), try again: ")
+
+def calculate_negatives(target:list):
+    # Calculate the amount of negative numbers in a list
+    result = 0
+    for number in target:
+        if number < 0:
+            result += 1
+
+    return result
+
+def main():
+    input_list:list = []
+    # Ask for user input until 0 is entered
+    while True:
+        user_input = get_input()
+        if user_input == 0:
+            break
+
+        input_list.append(user_input)
+
+    negatives = calculate_negatives(input_list)
+    evens = calculate_evens(input_list)
+    print(f"There were {negatives} negative numbers and {evens} even numbers in the list.")
+
+# Task 5
+def calculate_evens(target:list):
+    # Calculate the amount of even numbers in a list
+    result = 0
+    for number in target:
+        if number % 2 == 0:
+            result += 1
+    return result
+
+if __name__ == "__main__":
+    main()
