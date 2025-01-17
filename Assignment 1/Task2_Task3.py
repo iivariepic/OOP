d535fa9980fae8c14f791be0316b418c8c121ae5
Iivari Anttila <157590564+iivariepic@users.noreply.github.com>
2025-01-17 10:22:29 +0200

    Made the loose pieces of code inside main function

--- Assignment 1/Task2_Task3.py
+++ Assignment 1/Task2_Task3.py
@@ -1,8 +1,3 @@
-# Task 2
-print("Task 2")
-integer_list:list = []
-string_list:list = []
-
 # Filling with user inputs
 def input_to_list(target:list, target_type:type):
     # Function to fill a list with user inputs with the target type
@@ -30,27 +25,36 @@ def fill_list(target:list, target_type:type, amount:int = 10):
         print(f"Input {i+1}/{amount}")
         input_to_list(target, target_type)
 
-print("Filling integer list:")
-fill_list(integer_list, int)
-print("Filling string list:")
-fill_list(string_list, str)
-print("Integer list:", integer_list)
-print("String list:", string_list)
+def main():
+    # Task 2
+    print("Task 2")
+    integer_list: list = []
+    string_list: list = []
 
-# Filling with random numbers
-input("Press Enter to Continue...")
-import random
+    print("Filling integer list:")
+    fill_list(integer_list, int)
+    print("Filling string list:")
+    fill_list(string_list, str)
+    print("Integer list:", integer_list)
+    print("String list:", string_list)
 
-print("Filling integer list with random numbers")
-for x in range(10):
-    integer_list.append(random.randint(0,100))
-print("Here's the list now:", integer_list)
+    # Filling with random numbers
+    input("Press Enter to Continue...")
+    import random
 
-# Task 3
-input("Press Enter to Continue...")
-print("Task 3")
+    print("Filling integer list with random numbers")
+    for x in range(10):
+        integer_list.append(random.randint(0, 100))
+    print("Here's the list now:", integer_list)
 
-sorted_integer_list = sorted(integer_list)
-sorted_string_list = sorted(string_list)
-print("Integer list sorted:", sorted_integer_list)
-print("String list sorted:", sorted_string_list)
+    # Task 3
+    input("Press Enter to Continue...")
+    print("Task 3")
+
+    sorted_integer_list = sorted(integer_list)
+    sorted_string_list = sorted(string_list)
+    print("Integer list sorted:", sorted_integer_list)
+    print("String list sorted:", sorted_string_list)
+
+if __name__ == "__main__":
+    main()
