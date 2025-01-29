48e9a6e646fdcb6af7215e5de255d19e244777ad
Iivari Anttila <157590564+iivariepic@users.noreply.github.com>
2025-01-29 14:45:24 +0200

    Task 7 Complete

---
+++ Assignment 2/Task7.py
@@ -0,0 +1,15 @@
+class Pet:
+    def __init__(self, name:str, species:str, year_of_birth:int):
+        self.name = name
+        self.species = species
+        self.year_of_birth = year_of_birth
+
+def new_pet(name:str, species:str, year_of_birth:int):
+    return Pet(name, species, year_of_birth)
+
+def main():
+    doggy = new_pet("Doggy", "Cat", 1998)
+    print(doggy.name, doggy.species, doggy.year_of_birth)
+
+if __name__ == '__main__':
+    main()
