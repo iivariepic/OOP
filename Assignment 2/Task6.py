41aedfb83c6c729cf56e93cc75f833616e7cfca4
Iivari Anttila <157590564+iivariepic@users.noreply.github.com>
2025-01-29 14:38:38 +0200

    Task 6 complete

---
+++ Assignment 2/Task6.py
@@ -0,0 +1,18 @@
+class Movie:
+    def __init__(self, name, director,genre, year):
+        self.name = name
+        self.director = director
+        self.genre = genre
+        self.year = year
+
+def main():
+    EEAAO = Movie("Everything Everywhere All at Once", "Daniel Kwan",
+          "Surreal Drama", 2022)
+    FBDO = Movie("Ferris Bueller's Day Off", "John Hughes",
+                 "Comedy", 1986)
+
+    print(f"{EEAAO.director}: {EEAAO.name}, {EEAAO.year}")
+    print(f"The Genre of the movie {FBDO.name} is {FBDO.genre}")
+
+if __name__ == "__main__":
+    main()
