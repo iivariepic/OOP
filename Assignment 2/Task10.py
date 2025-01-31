631d0e3c0ace66dc6b46fc081977e9d404ff2007
Iivari Anttila <157590564+iivariepic@users.noreply.github.com>
2025-01-31 11:54:04 +0200

    Part 2 complete

--- Assignment 2/Task10.py
+++ Assignment 2/Task10.py
@@ -8,14 +8,21 @@ class RealProperty:
         return self.square_metres > compared_to.square_metres
 
     def price_difference(self, compared_to:"RealProperty") -> int:
-        pass
+        self_price = (self.price_per_sqm * self.square_metres)
+        comparison_price = (compared_to.price_per_sqm * compared_to.square_metres)
+        return abs(self_price - comparison_price)
 
-def main():
+
+def main() -> None:
     central_studio = RealProperty(1, 16, 5500)
     downtown_two_bedroom = RealProperty(2, 38, 4200)
     suburbs_three_bedroom = RealProperty(3, 78, 2500)
+
     print(central_studio.bigger(downtown_two_bedroom))
     print(suburbs_three_bedroom.bigger(downtown_two_bedroom))
+
+    print(central_studio.price_difference(downtown_two_bedroom))
+    print(suburbs_three_bedroom.price_difference(downtown_two_bedroom))
 
 if __name__ == "__main__":
     main()
