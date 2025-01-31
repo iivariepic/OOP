10f8494a092348ac18fd95c673253efb5a49a803
Iivari Anttila <iivari03@gmail.com>
2025-02-01 00:25:54 +0200

    Added type hints

--- Assignment 2/Task4.py
+++ Assignment 2/Task4.py
@@ -1,17 +1,17 @@
-def factorials(n:int):
+def factorials(n:int) -> dict:
     result = {}
     for current_factorial in range(1,n+1):
         result[current_factorial] = factorial(current_factorial)
     return result
 
-def factorial(n):
+def factorial(n) -> int:
     result = 1
     for current_multiplier in range(n, 0, -1):
         result = result * current_multiplier
 
     return result
 
-def main():
+def main() -> None:
     k = factorials(5)
     print(k[1])
     print(k[3])
