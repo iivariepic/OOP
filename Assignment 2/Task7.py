652e3a6e2b5240fea6f2ac33c7ce6fbab060a90e
Iivari Anttila <iivari03@gmail.com>
2025-02-01 00:28:33 +0200

    added type hints

--- Assignment 2/Task7.py
+++ Assignment 2/Task7.py
@@ -4,10 +4,10 @@ class Pet:
         self.species = species
         self.year_of_birth = year_of_birth
 
-def new_pet(name:str, species:str, year_of_birth:int):
+def new_pet(name:str, species:str, year_of_birth:int) -> Pet:
     return Pet(name, species, year_of_birth)
 
-def main():
+def main() -> None:
     doggy = new_pet("Doggy", "Cat", 1998)
     print(doggy.name, doggy.species, doggy.year_of_birth)
 
