5f264e4d9a8b315f8f5271206082756e653b695c
Iivari Anttila <iivari03@gmail.com>
2025-02-01 00:27:45 +0200

    Added type hints and played with values

--- Assignment 2/Task5.py
+++ Assignment 2/Task5.py
@@ -1,4 +1,4 @@
-def smallest_average(person1:dict, person2: dict, person3: dict):
+def smallest_average(person1:dict, person2: dict, person3: dict) -> dict:
     person1_average = (calculate_average(get_scores(person1)), person1)
     person2_average = (calculate_average(get_scores(person2)), person2)
     person3_average = (calculate_average(get_scores(person3)), person3)
@@ -7,26 +7,26 @@ def smallest_average(person1:dict, person2: dict, person3: dict):
     smallest = sorted_averages[0]
     return smallest[1]
 
-def calculate_average(score_list:list):
+def calculate_average(score_list:list) -> float:
     result = 0
     for score in score_list:
         result += score
     return result/len(score_list)
 
-def get_scores(person:dict):
+def get_scores(person:dict) -> list:
     return [person["result1"], person["result2"], person["result3"]]
 
-def main():
+def main() -> None:
     person1 = {
         "name": "John",
-        "result1": 1,
+        "result1": 9,
         "result2": 6,
         "result3": 3
     }
     person2 = {
         "name": "Mike",
         "result1": 4,
-        "result2": 5,
+        "result2": 0,
         "result3": 7
     }
     person3 = {
