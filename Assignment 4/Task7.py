5f96b9d376c119a21e374e72333e39cabdb9a8b7
Iivari Anttila <157590564+iivariepic@users.noreply.github.com>
2025-02-26 14:52:45 +0200

    Task 7 part 1

---
+++ Assignment 4/Task7.py
@@ -0,0 +1,42 @@
+class Person:
+    def __init__(self, name, height):
+        self.name = name
+        self.height = height
+
+    def __str__(self):
+        return f"{self.name} ({self.height})"
+
+class Room:
+    def __init__(self):
+        self.people:list[Person] = []
+
+    def add(self, person: Person):
+        self.people.append(person)
+
+    def is_empty(self):
+        return len(self.people) == 0
+
+    def combined_height(self):
+        result = 0
+        for person in self.people:
+            result += person.height
+        return result
+
+    def print_contents(self):
+        print(f"There are {len(self.people)} persons in the room, and their combined height is {self.combined_height()} cm")
+        for person in self.people:
+            print(person)
+
+def main():
+    room = Room()
+    print("Is the room empty?", room.is_empty())
+    room.add(Person("Lea", 183))
+    room.add(Person("Kenya", 172))
+    room.add(Person("Ally", 166))
+    room.add(Person("Nina", 162))
+    room.add(Person("Dorothy", 175))
+    print("Is the room empty?", room.is_empty())
+    room.print_contents()
+
+if __name__ == '__main__':
+    main()
