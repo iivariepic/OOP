3ee6544a4fbf9e536495baa60624520197593654
Iivari Anttila <157590564+iivariepic@users.noreply.github.com>
2025-03-12 14:05:06 +0200

    LongestWord Complete

---
+++ Assignment 5/Task 6.py
@@ -0,0 +1,56 @@
+import random
+
+class WordGame:
+    def __init__(self, rounds: int):
+        self.wins1 = 0
+        self.wins2 = 0
+        self.rounds = rounds
+
+    def round_winner(self, player1_word: str, player2_word: str):
+        # determine a random winner
+        return random.randint(1, 2)
+
+    def play(self):
+        print("Word game:")
+        for i in range(1, self.rounds+1):
+            print(f"round {i}")
+            answer1 = input("player1: ")
+            answer2 = input("player2: ")
+
+            if self.round_winner(answer1, answer2) == 1:
+                self.wins1 += 1
+                print("player 1 won")
+            elif self.round_winner(answer1, answer2) == 2:
+                self.wins2 += 1
+                print("player 2 won")
+            else:
+                pass # it's a tie
+
+        print("game over, wins:")
+        print(f"player 1: {self.wins1}")
+        print(f"player 2: {self.wins2}")
+
+
+class LongestWord(WordGame):
+    def __init__(self, rounds: int):
+        super().__init__(rounds)
+
+    def round_winner(self, player1_word: str, player2_word: str):
+        player1Length = len(player1_word)
+        player2Length = len(player2_word)
+
+        if player1Length == player2Length:
+            return 0
+        elif player1Length > player2Length:
+            return 1
+        else:
+            return 2
+
+
+
+def main():
+    p = LongestWord(3)
+    p.play()
+
+if __name__ == '__main__':
+    main()
