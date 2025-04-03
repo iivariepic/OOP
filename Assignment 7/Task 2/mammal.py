5ce01d91cba89758204a1413763c75eb08c5b3c3
Iivari Anttila <iivari03@gmail.com>
2025-04-03 23:35:53 +0300

    Part 4 complete

---
+++ Assignment 7/Task 2/mammal.py
@@ -0,0 +1,28 @@
+class Mammal()
+    def __init__(self, ID:int, species:str, name:str,
+                 size:float, weight:float):
+        self.__ID = ID
+        self.__species = species
+        self.__name = name
+        self.__size = size
+        self.__weight = weight
+
+    @property
+    def ID(self):
+        return self.__ID
+
+    @property
+    def species(self):
+        return self.__species
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def size(self):
+        return self.__size
+
+    @property
+    def weight(self):
+        return self.__weight
