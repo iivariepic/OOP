38fc316c3c263970b5c7207c087ae42f7ae8d477
Iivari Anttila <iivari03@gmail.com>
2025-04-04 00:15:45 +0300

    Task 2 Complete

--- Assignment 7/Task 2/game.py
+++ Assignment 7/Task 2/game.py
@@ -6,28 +6,51 @@ def game(playerdict:dict):
     result_dict:dict = {}
 
     for player in playerdict.keys():
-        dice = playerdict[player]
-        dice.roll()
-        result_dict[player] = dice.sideup
+        result_dict[player] = 0
+
+        for dice in playerdict[player]:
+            dice.roll()
+            result_dict[player] += dice.sideup
 
     return result_dict
 
 def main():
-   player1 = Player("A", 1)
-   player2 = Player("B", 2)
-   player3 = Player("C", 3)
-   player1.pet = Mammal(1, "Dog", "Haukku", 10, 100)
-   player2.pet = Mammal(2, "Cat", "Miukku", 8, 80)
-   player3.pet = Mammal(3, "Gerbil", "Germa", 2, 20)
+    player1 = Player("A", 1)
+    player2 = Player("B", 2)
+    player3 = Player("C", 3)
+    pet1 = Mammal(3, "Gerbil", "Germa", 2, 20)
+    pet2 = Mammal(2, "Cat", "Miukku", 8, 80)
+    pet3 = Mammal(1, "Dog", "Haukku", 10, 100)
 
-   players:dict = {
-       player1: Dice(),
-       player2: Dice(),
-       player3: Dice(),
-   }
+    players:dict = {
+       player1: [Dice(), Dice()],
+       player2: [Dice(), Dice()],
+       player3: [Dice(), Dice()],
+    }
 
-   result:dict = game(players)
-   print(f"The winner is {max(result, key=result.get)} with a score of {max(result.values())}")
+    ## Pet selection
+    print("Selecting Pets")
+    for player in players:
+       for dice in players[player]:
+           dice.roll()
+
+    playersums: dict = {}
+    for player in players:
+        player_sum: int = 0
+        for dice in players[player]:
+            player_sum += dice.sideup
+
+        playersums[player] = player_sum
+
+    sorted_rolls = sorted(players, key=playersums.get, reverse=True)
+    sorted_rolls[0].pet = pet3
+    sorted_rolls[1].pet = pet2
+    sorted_rolls[2].pet = pet1
+
+
+    # Run game
+    result:dict = game(players)
+    print(f"The winner is {max(result, key=result.get)} with a score of {max(result.values())}")
 
 
 
